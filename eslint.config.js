5a6c87f9a57283073db8559f4e1b8d36bd11eb1f
siyeon <aeiou0302@icloud.com>
2025-03-18 16:36:24 +0900

    chore: ESLint 설정에서 dist 디렉토리 제외

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,31 @@ import eslintPluginPrettier from 'eslint-plugin-prettier'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import eslintPluginReact from 'eslint-plugin-react'
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
+import js from '@eslint/js'
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js'
+import globals from 'globals'
 
 export default [
+  js.configs.recommended,
+  reactRecommended,
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    ignores: ['dist/**/*'],
     plugins: {
       prettier: eslintPluginPrettier,
       react: eslintPluginReact,
       'react-hooks': eslintPluginReactHooks,
     },
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
       parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
         ecmaFeatures: {
           jsx: true,
         },
+      },
+      globals: {
+        ...globals.browser,
       },
     },
     settings: {
